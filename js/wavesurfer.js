3db66f5d8d8dc51b5c16e5de7015ea79066276a5
Jessie Duan <jduan48@gmail.com>
2015-05-28 00:49:59 -0700

    initial commit

---
+++ js/wavesurfer.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// Create an instance
+var wavesurfer = Object.create(WaveSurfer);
+
+// Init & load audio file
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        container     : document.querySelector('#waveform'),
+        waveColor     : 'violet',
+        progressColor : 'purple',
+        loaderColor   : 'purple',
+        cursorColor   : 'navy'
+    };
+
+    if (location.search.match('scroll')) {
+        options.minPxPerSec = 100;
+        options.scrollParent = true;
+    }
+
+    // Init
+    wavesurfer.init(options);
+    // Load audio from URL
+    wavesurfer.load('audio/brain.mp3');
+
+});
+
+/* Progress bar */
+document.addEventListener('DOMContentLoaded', function () {
+    var progressDiv = document.querySelector('#progress-bar');
+    var progressBar = progressDiv.querySelector('.progress-bar');
+
+    var showProgress = function (percent) {
+        progressDiv.style.display = 'block';
+        progressBar.style.width = percent + '%';
+    };
+
+    var hideProgress = function () {
+        progressDiv.style.display = 'none';
+    };
+
+    wavesurfer.on('loading', showProgress);
+    wavesurfer.on('ready', hideProgress);
+    wavesurfer.on('destroy', hideProgress);
+    wavesurfer.on('error', hideProgress);
+});
+
+var GLOBAL_ACTIONS = {
+    'play': function () {
+        wavesurfer.playPause();
+    },
+};
+
+// Bind actions to buttons
+document.addEventListener('DOMContentLoaded', function () {
+    [].forEach.call(document.querySelectorAll('[data-action]'), function (el) {
+        el.addEventListener('click', function (e) {
+            var action = e.currentTarget.dataset.action;
+            if (action in GLOBAL_ACTIONS) {
+                e.preventDefault();
+                GLOBAL_ACTIONS[action](e);
+            }
+        });
+    });
+});
+
+
+// Misc
+document.addEventListener('DOMContentLoaded', function () {
+    // Navbar links
+    var ul = document.querySelector('.nav-pills');
+    var pills = ul.querySelectorAll('li');
+    var active = pills[0];
+    if (location.search) {
+        var first = location.search.split('&')[0];
+        var link = ul.querySelector('a[href="' + first + '"]');
+        if (link) {
+            active =  link.parentNode;
+        }
+    }
+    active && active.classList.add('active');
+});
+
